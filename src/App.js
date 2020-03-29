9cec005973dcf819d925b75a382d849ed6e1af56
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-03-29 19:57:21 -0400

    fix protected routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
+import ProtectedRoute from './utils/ProtectedRoute';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/profile">
-          <h1>Profile</h1>
-        </Route>
-        <Route path="/profile">
-          <h1>Profile</h1>
-        </Route>
-      </Switch>
+      <Route exact path="/" component={Home} />
+      <ProtectedRoute
+        exact
+        path="/profile"
+        render={(props) => <h1>Profile</h1>}
+      />
     </>
   );
 }
